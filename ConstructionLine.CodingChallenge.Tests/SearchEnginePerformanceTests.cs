76ca9472b1ac61615c255dc6240b9c66569bb4f8
J B <jonbreen@outlook.com>
2020-07-15 12:54:56 +0100

    Collapsed correctness tests into perfrormance and added tests to verify shirt mapping

--- ConstructionLine.CodingChallenge.Tests/SearchEnginePerformanceTests.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEnginePerformanceTests.cs
@@ -14,39 +14,27 @@ namespace ConstructionLine.CodingChallenge.Tests
         {
             Shirts = Fixture.CreateMany<Shirt>(50000).ToList();
         }
-        
+
         [Test]
-        public void PerformanceTest_WithColorSearchOptions_ExecutesWithinTime()
+        public void PerformanceTest_WithColorSearchOptions_ExecutesCorrectlyWithinTime()
         {
             // arrange
             var searchOptions = new SearchOptions
             {
                 Colors = Fixture.CreateMany<Color>().ToList()
             };
+            SearchResults results = null;
             // act
-            Action act = () => ClassUnderTest.Search(searchOptions);
+            Action act = () => results = ClassUnderTest.Search(searchOptions);
             // assert
-            act.ExecutionTime().Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
-        }
-        
-        [Test]
-        public void PerformanceTest_WithColorSearchOptions_ResultsAreCorrect()
-        {
-            // arrange
-            var searchOptions = new SearchOptions
-            {
-                Colors = Fixture.CreateMany<Color>().ToList()
-            };
-            // act
-            var results = ClassUnderTest.Search(searchOptions);
-            // assert
+            act.ExecutionTimeOf(s => s()).Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
             AssertResults(searchOptions, results.Shirts);
             AssertColorCounts(searchOptions, results.ColorCounts);
             AssertSizeCounts(searchOptions, results.SizeCounts);
         }
-
+        
         [Test]
-        public void PerformanceTest_WithSizeAndColorSearchOptions_ExecutesWithinTime()
+        public void PerformanceTest_WithSizeAndColorSearchOptions_ExecutesCorreclyWithinTime()
         {
             // arrange
             var searchOptions = new SearchOptions
@@ -54,54 +42,29 @@ namespace ConstructionLine.CodingChallenge.Tests
                 Colors = Fixture.CreateMany<Color>().ToList(),
                 Sizes = Fixture.CreateMany<Size>().ToList()
             };
+            SearchResults results = null;
             // act
-            Action act = () => ClassUnderTest.Search(searchOptions);
+            Action act = () => results = ClassUnderTest.Search(searchOptions);
             // assert
-            act.ExecutionTime().Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
-        }
-
-        [Test]
-        public void PerformanceTest_WithSizeAndColorSearchOptions_ResultsAreCorrect()
-        {
-            // arrange
-            var searchOptions = new SearchOptions
-            {
-                Colors = Fixture.CreateMany<Color>().ToList(),
-                Sizes = Fixture.CreateMany<Size>().ToList()
-            };
-            // act
-            var results = ClassUnderTest.Search(searchOptions);
-            // assert
+            act.ExecutionTimeOf(s => s()).Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
             AssertResults(searchOptions, results.Shirts);
             AssertColorCounts(searchOptions, results.ColorCounts);
             AssertSizeCounts(searchOptions, results.SizeCounts);
         }
-
+        
         [Test]
-        public void PerformanceTest_WithSizeSearchOptions_ExecutesWithinTime()
+        public void PerformanceTest_WithSizeSearchOptions_ExecutesCorrectlyWithinTime()
         {
             // arrange
             var searchOptions = new SearchOptions
             {
                 Sizes = Fixture.CreateMany<Size>().ToList()
             };
+            SearchResults results = null;
             // act
-            Action act = () => ClassUnderTest.Search(searchOptions);
+            Action act = () => results = ClassUnderTest.Search(searchOptions);
             // assert
-            act.ExecutionTime().Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
-        }
-
-        [Test]
-        public void PerformanceTest_WithSizeSearchOptions_ResultsAreCorrect()
-        {
-            // arrange
-            var searchOptions = new SearchOptions
-            {
-                Sizes = Fixture.CreateMany<Size>().ToList()
-            };
-            // act
-            var results = ClassUnderTest.Search(searchOptions);
-            // assert
+            act.ExecutionTimeOf(s => s()).Should().BeLessOrEqualTo(TimeSpan.FromMilliseconds(100));
             AssertResults(searchOptions, results.Shirts);
             AssertColorCounts(searchOptions, results.ColorCounts);
             AssertSizeCounts(searchOptions, results.SizeCounts);
--- ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AutoFixture;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace ConstructionLine.CodingChallenge.Tests
@@ -102,6 +103,26 @@ namespace ConstructionLine.CodingChallenge.Tests
         }
 
         [Test]
+        public void Search_WithNonMatchingScenario_ResultsAreCorrect()
+        {
+            // arrange
+            Shirts = new List<Shirt>
+            {
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Large).With(x => x.Color, Color.Red).Create(),
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Medium).With(x => x.Color, Color.Blue).Create()
+            };
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> {Size.Small},
+                Colors = new List<Color> {Color.Black}
+            };
+            // act
+            var results = ClassUnderTest.Search(searchOptions);
+            // assert
+            results.Shirts.Should().BeEmpty();
+        }
+
+        [Test]
         public void Search_WithNoSearchOption_ColorCountsAreCorrect()
         {
             // arrange
@@ -132,6 +153,78 @@ namespace ConstructionLine.CodingChallenge.Tests
             var results = ClassUnderTest.Search(searchOptions);
             // assert
             AssertSizeCounts(searchOptions, results.SizeCounts);
+        }
+
+        [Test]
+        public void Search_WithOneMatching_MapsShirtId()
+        {
+            // arrange
+            var matchingShirt = Fixture.Build<Shirt>().With(x => x.Size, Size.Small).With(x => x.Color, Color.Black)
+                .Create();
+            Shirts = new List<Shirt>
+            {
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Large).With(x => x.Color, Color.Red).Create(),
+                matchingShirt,
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Medium).With(x => x.Color, Color.Blue).Create()
+            };
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> {Size.Small},
+                Colors = new List<Color> {Color.Black}
+            };
+            // act
+            var results = ClassUnderTest.Search(searchOptions);
+            // assert
+            results.Shirts.Should().HaveCount(1);
+            results.Shirts.Single().Id.Should().Be(matchingShirt.Id);
+        }
+
+        [Test]
+        public void Search_WithOneMatching_MapsShirtName()
+        {
+            // arrange
+            var matchingShirt = Fixture.Build<Shirt>().With(x => x.Size, Size.Small).With(x => x.Color, Color.Black)
+                .Create();
+            Shirts = new List<Shirt>
+            {
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Large).With(x => x.Color, Color.Red).Create(),
+                matchingShirt,
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Medium).With(x => x.Color, Color.Blue).Create()
+            };
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> {Size.Small},
+                Colors = new List<Color> {Color.Black}
+            };
+            // act
+            var results = ClassUnderTest.Search(searchOptions);
+            // assert
+            results.Shirts.Should().HaveCount(1);
+            results.Shirts.Single().Name.Should().BeSameAs(matchingShirt.Name).And.NotBeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public void Search_WithOneMatching_ResultsAreCorrect()
+        {
+            // arrange
+            var matchingShirt = Fixture.Build<Shirt>().With(x => x.Size, Size.Small).With(x => x.Color, Color.Black)
+                .Create();
+            Shirts = new List<Shirt>
+            {
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Large).With(x => x.Color, Color.Red).Create(),
+                matchingShirt,
+                Fixture.Build<Shirt>().With(x => x.Size, Size.Medium).With(x => x.Color, Color.Blue).Create()
+            };
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> {Size.Small},
+                Colors = new List<Color> {Color.Black}
+            };
+            // act
+            var results = ClassUnderTest.Search(searchOptions);
+            // assert
+            results.Shirts.Should().HaveCount(1);
+            results.Shirts.Should().BeEquivalentTo(new List<Shirt> {matchingShirt});
         }
 
         [Test]
